4343fbbe85c1860e83603896f4ca2e9fa0893e9b
Giulio Mazzi <mazzi.giulio2@gmail.com>
2018-04-23 14:58:02 +0200

    improved readability

--- scripts/merge_pickle.py
+++ scripts/merge_pickle.py
@@ -1,15 +1,30 @@
+"""
+merge pickle file created by the dataset_generator and located inside
+the dataset folder
+"""
 import pickle
-import glob, os
+import glob
+import os
 
-os.chdir("../dataset")
-merged_vect = []
-for f in glob.glob("*.pkl"):
-    with open(f, 'rb') as pkl:
-        for i in pickle.load(pkl):
-            merged_vect.append(i)
 
-with open('merged.pkl', 'wb') as pkl:
-    pickle.dump(merged_vect, pkl)
+def main():
+    """merge file inside dataset"""
+    os.chdir("../dataset")
+    merged_vect = []
 
-for f in glob.glob("dataset*.pkl"):
-    os.remove(f)
+    # read all the dataset file
+    for pkl in glob.glob("*.pkl"):
+        with open(pkl, 'rb') as pkl:
+            for i in pickle.load(pkl):
+                merged_vect.append(i)
+
+    # merge everything inside a single file
+    with open('merged.pkl', 'wb') as pkl:
+        pickle.dump(merged_vect, pkl)
+
+    # remove old dataset
+    for dataset in glob.glob("dataset*.pkl"):
+        os.remove(dataset)
+
+if __name__ == "__Main__":
+    main()
